11f5f6cf5fb57438aa89e27047ecf193e6a5cbfb
DaavidM <113436157+DaavidM9403@users.noreply.github.com>
2022-12-28 11:36:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+exit = input("Do you want to exit?:")
+while exit != "Yes":
+   Fave = input("What is you favorite animal?:")
+   if Fave == "cow":
+    print("A cow make the sound moooo")
+   if Fave == "pig":
+       print("a pig make the sound oink, oink")
+   if Fave == "dog":
+    print("roof, roof, roof, roof")
+   if Fave == "bear":
+    print(" a bear makes the sound growl")
+   exit = input("Do you want to exit?:")
+    
+
+
+    
+    
+
+  
+  
+    
+
+
+
+
+
+
+
+
+
